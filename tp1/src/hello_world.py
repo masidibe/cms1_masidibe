74d23db15d049eeefddec7115d0f1949921c482c
Mamadou Sidibe <masidibe@emi.u-bordeaux1.fr>
2018-09-18 11:15:46 +0200

    premier commit : ajout des documents precedents

---
+++ tp1/src/hello_world.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# utiliser utf-8 pour afficher les accents français (comme sous LaTeX)
+
+print("Une chaîne de caractère avec des caractères typiquement français, par exemple le symbole de l'euro, € !")
